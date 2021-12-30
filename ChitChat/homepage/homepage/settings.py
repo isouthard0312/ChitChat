8ce42722a46ac5961d186fd72a0f45a6d34d3bdc
Isabella Southard <70180041+isouthard0312@users.noreply.github.com>
2021-12-30 08:29:39 -0600

    update

--- ChitChat/homepage/homepage/settings.py
+++ ChitChat/homepage/homepage/settings.py
@@ -11,8 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import dj_database_url
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-spjn@gx@3z5&suzvn33us@up8##in1id5e_((5ttr(b6ar*&r*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -68,8 +66,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'homepage.urls'
@@ -98,7 +95,7 @@ WSGI_APPLICATION = 'homepage.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
@@ -160,5 +157,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #sends all emai
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / 'media'
 
-import django_heroku
-django_heroku.settings(locals())
